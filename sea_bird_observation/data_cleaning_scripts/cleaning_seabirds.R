39a48b85d555dece3e5d1eb7a5cb6b4f9dd52b64
Thijmen18 <36414676+Thijmen18@users.noreply.github.com>
2023-06-10 14:22:18 +0100

    add code to clean column names

--- sea_bird_observation/data_cleaning_scripts/cleaning_seabirds.R
+++ sea_bird_observation/data_cleaning_scripts/cleaning_seabirds.R
@@ -2,3 +2,44 @@
 # this script contains code to clean the raw data 'seabirds.xls'
 
 ####### open & exploring data
+library(tidyverse)
+library(readxl)
+
+# xls has 2 worksheets: open separately
+
+seabirds <- read_excel("raw_data/seabirds.xls", sheet = "Bird data by record ID")
+
+ships <- read_excel("raw_data/seabirds.xls", sheet = "Ship data by record ID")
+
+names(seabirds)
+glimpse(seabirds)
+head(seabirds)
+names(ships)
+glimpse(ships)
+head(ships)
+
+# lets first convert/clean the names
+
+ships_names <- janitor::clean_names(ships)
+
+seabirds_names <- seabirds %>% 
+  rename("species_common" = "Species common name (taxon [AGE / SEX / PLUMAGE PHASE])",
+         "species_scientific" = "Species  scientific name (taxon [AGE /SEX /  PLUMAGE PHASE])"
+         ) %>% 
+janitor::clean_names()
+seabirds_names
+
+# before joining ship and bird tables, lets drop non-informative columns (for our analysis)
+
+seabirds_reduced <- seabirds_names %>% 
+  select(record, record_id, species_common, species_scientific,
+         species_abbreviation, age, count, sex)
+
+ships_reduced <- ships_names %>% 
+  select(record, record_id, date, time, lat, long, ew, prec, wspeed, cld, seasn, latcell)
+
+#both ships and seabird dataset have record numbering, lets rename to avoid confusion
+
+seabirds_renamed <- rename(seabirds_reduced, "recordnr_seabird" = "record")
+
+ships_renamed <- rename(ships_reduced, "recordnr_ships" = "record")
